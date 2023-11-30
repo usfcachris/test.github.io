8cf6df7c58cc4fd6d22612ed0a63ac8a700e1ef1
earthIsRound9 <244628489c@gmail.com>
2023-11-29 20:56:02 -0800

    animation added

---
+++ js/animation_nav.js
@@ -0,0 +1,12 @@
+
+/**
+$(function() {
+    $("nav").mouseover(fMouseOver);
+});
+
+function fMouseOver() {
+    $(this).animate({height: "150px"}, 600);
+    $(this).mouseleave(fMouseOut);
+    
+}
+ */
